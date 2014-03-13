a87c677c4682685e6d4ad7135deabb648f4206f2
Mikael Hernrup <mikael@hernrup.se>
2014-03-13 02:19:12 +0100

    Documentation

--- mkdocs/search.py
+++ mkdocs/search.py
@@ -4,16 +4,21 @@ import json
 import re
 from HTMLParser import HTMLParser
 
+#dataholder for index
 class SearchIndex(object):
     def __init__(self):
         self.pages = []
 
-
+    #add entry based on predetermined properties
     def addEntryFromContext(self, page, content, nav, toc, meta, config):
 
+        #create parser for analysing content
+        #we parse the content since the toc dont have the data
+        #and we need to use toc urls
         parser = ContentParser()
         parser.feed(content)
 
+        #create entry for page
         self.createEntry(
             title=page.title,
             text=self.strip_tags(content).rstrip('\n'),
@@ -21,18 +26,25 @@ class SearchIndex(object):
             loc=page.abs_url
         )
 
+        #check all found sections against toc, match on id
         for section in parser.data:
+            #toc h1
             for toc_item in toc:
+                #dont check sub sections if found
                 if toc_item.url[1:] == section.id and len(section.text) > 0:
+                    #create entry
                     self.createEntry(
                         title=toc_item.title,
                         text=" ".join(section.text),
                         tags="",
                         loc=page.abs_url[:-1] + toc_item.url
                     )
+                #not fund, check h2
                 else:
+                    #toc h2
                     for toc_sub_item in toc_item.children:
                         if toc_sub_item.url[1:] == section.id and len(section.text) > 0:
+                            #create entry
                             self.createEntry(
                                 title=toc_sub_item.title,
                                 text=" ".join(section.text),
@@ -40,6 +52,7 @@ class SearchIndex(object):
                                 loc=page.abs_url[:-1] + toc_sub_item.url
                             )
 
+    #create a index entry
     def createEntry(self, title,text,tags,loc):
         entry = SearchEntry(
             title=title,
@@ -49,18 +62,17 @@ class SearchIndex(object):
         )
         self.pages.append(entry)
 
-    def to_json(self):
+    #python to JSON conversion
+    def generate_search_index(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
-    def generate_search_index(self):
-        s = self.to_json()
-        return s
-
+    #strip html tags from data
     def strip_tags(self,html):
         s = MLStripper()
         s.feed(html)
         return s.get_data()
 
+#data container for a index entry
 class SearchEntry(object):
     def __init__(self, title, text, tags, loc):
         self.title = title
@@ -68,6 +80,7 @@ class SearchEntry(object):
         self.tags = tags
         self.loc = loc
 
+#class for stripping html tags
 class MLStripper(HTMLParser):
     def __init__(self):
         self.reset()
@@ -77,6 +90,7 @@ class MLStripper(HTMLParser):
     def get_data(self):
         return ''.join(self.fed)
 
+#class for parsing html-sections
 class ContentParser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
@@ -85,6 +99,7 @@ class ContentParser(HTMLParser):
         self.section = ""
         self.is_header_tag = False
 
+    #hook - tag start
     def handle_starttag(self, tag, attrs):
         if tag in ("h1","h2"):
             self.is_header_tag = True
@@ -93,18 +108,20 @@ class ContentParser(HTMLParser):
                 if attr[0] == "id":
                     self.section.id = attr[1]
 
+    #hook - tag end
     def handle_endtag(self, tag):
         if tag in ("h1","h2"):
             self.is_header_tag = False
             self.data.append(self.section)
 
+    #hook - data
     def handle_data(self, data):
         if self.is_header_tag:
             self.section.title = data
         else:
             self.section.text.append(data.rstrip('\n'))
 
-
+#content-holder for html-sections
 class ContentSection():
      def __init__(self):
         self.text = []
