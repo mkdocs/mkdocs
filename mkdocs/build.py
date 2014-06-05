abb9d82ade6bf143d7dc154cf56053bd14d664ae
jstephens <jstephens@jstephens-test2-centos64.dyn.yojoe.local>
2014-06-04 21:40:19 -0700

    Making search work for readthedocs theme.  It borrows client side code from sphinx but has no dependency on that project.

--- mkdocs/build.py
+++ mkdocs/build.py
@@ -2,12 +2,16 @@
 
 from mkdocs import nav, toc, utils
 from urlparse import urljoin
-import jinja2
-import markdown
-import os
-import re
-import urlparse
+from pprint import pprint as pp
+import jinja2, markdown, os, re
+import urlparse, json
 
+os.environ['NLTK_DATA'] = os.path.expanduser("~") + '/nltk_data'
+from nltk.corpus import stopwords
+from nltk import PorterStemmer
+
+badwords = stopwords.words("english")
+stemmer = PorterStemmer()
 
 class PathToURL(object):
     def __init__(self, nav=None):
@@ -131,6 +135,85 @@ def get_context(page, content, nav, toc, meta, config):
         'include_search': config['include_search'],
     }
 
+def generate_search(config,index,env,site_nav):
+    # output index file
+    utils.write_file("Search.setIndex(" + json.dumps(index) + ")", os.path.join(config['site_dir'], 'searchindex.js'))
+
+    template = env.get_template('search.html')
+    base = env.get_template('base.html')
+    ejs = config['extra_javascript']
+    ejs.append('js/searchtools.js')
+    content = base.render({
+            'site_name': config['site_name'],
+            'site_author': config['site_author'],
+            'site_favicon': None,
+            'page_title': 'Search',
+            'page_description': 'Search',
+            'content': template.render({}),
+            'toc': None,
+            'nav': site_nav,
+            'meta': None,
+            'config': config,
+            'base_url': site_nav.url_context.make_relative('/'),
+            'homepage_url': site_nav.homepage.url,
+            'canonical_url': None,
+            'current_page': None,
+            'previous_page': None,
+            'next_page': None,
+            'repo_url': config['repo_url'],
+            'repo_name': config['repo_name'],
+            'extra_css': config['extra_css'],
+            'extra_javascript': ejs,
+            'include_nav': config['include_nav'],
+            'include_next_prev': config['include_next_prev'],
+            'include_search': config['include_search'],
+        })
+    utils.write_file(content, os.path.join(config['site_dir'], 'search.html'))
+
+def generate_index(content):
+    # remove html comments
+    newcon = re.sub(r"<!--(.|\n|\r|\s)+?-->", "", content)
+    # fix links
+    newcon = re.sub(r"\([^\)]+\)", "", newcon)
+    # remove any extra junk...
+    newcon = re.sub(r"[^a-zA-Z0-9 \t\n]+", "", newcon)
+    words = [word for word in newcon.split() if word not in badwords]
+    output = set([])
+    # stemmer does some weird things, so while this can cause "duplicates" it's not that bad
+    for w in words:
+        output.add(w)
+        output.add(stemmer.stem_word(w))
+    return (output,newcon)
+
+def merge_index(index,title,url,page_index,title_index):
+    cur_index = len(index['filenames'])
+    index['filenames'].append(url)
+    index['titles'].append(title)
+
+    # build inverted index of page words
+    for t in page_index:
+        if t in index['terms']:
+            if isinstance( index['terms'][t], ( int, long ) ):
+                index['terms'][t] = [ index['terms'][t], cur_index ]
+            elif isinstance( index['terms'][t], list ):
+                index['terms'][t].append(cur_index)
+            else:
+                index['terms'][t] = cur_index
+        else:
+            index['terms'][t] = cur_index
+
+    # same thing, but for titles
+    for t in title_index:
+        if t in index['titleterms']:
+            if isinstance( index['titleterms'][t], ( int, long ) ):
+                index['titleterms'][t] = [ index['titleterms'][t], cur_index ]
+            elif isinstance( index['titleterms'][t], list ):
+                index['titleterms'][t].append(cur_index)
+            else:
+                index['titleterms'][t] = cur_index
+        else:
+            index['titleterms'][t] = cur_index
+
 
 def build_pages(config):
     """
@@ -140,15 +223,34 @@ def build_pages(config):
     loader = jinja2.FileSystemLoader(config['theme_dir'])
     env = jinja2.Environment(loader=loader)
 
+    # mirrors sphinx so we can reuse their client side code
+    index = {
+        'envversion': 42,
+        'terms': {},      # term => int/array of index
+        'objtypes': {},
+        'objnames': {},
+        'filenames': [],  # pageUrl
+        'titles': [],     # matching title
+        'objects': {},
+        'titleterms': {}  # term => int/array of index
+    }
+    # output =>  Searc.setIndex( ...  json dump ... )
     for page in site_navigation.walk_pages():
+        if page.title is None:
+            title = config['site_name']
+        else:
+            title = str(page.title)
         # Read the input file
         input_path = os.path.join(config['docs_dir'], page.input_path)
         input_content = open(input_path, 'r').read().decode('utf-8')
 
         # Process the markdown text
+        title_index, source = generate_index(title)
+        input_index, source = generate_index(input_content)
         html_content, table_of_contents, meta = convert_markdown(input_content)
         html_content = post_process_html(html_content, site_navigation)
 
+        merge_index(index,title,str(page.abs_url).rstrip('/') + '/index',input_index,title_index)
         context = get_context(
             page, html_content, site_navigation,
             table_of_contents, meta, config
@@ -166,6 +268,13 @@ def build_pages(config):
         # Write the output file.
         output_path = os.path.join(config['site_dir'], page.output_path)
         utils.write_file(output_content.encode('utf-8'), output_path)
+
+        if config['include_search']:
+            sources = os.path.join(config['site_dir'], '_sources/' + str(page.abs_url).rstrip('/') + '/index.txt')
+            utils.write_file(source, sources)
+
+    if config['include_search']:
+        generate_search(config,index,env,site_navigation)
 
 
 def build(config, live_server=False):
