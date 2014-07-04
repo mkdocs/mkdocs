20eda86df7379fd37fd983afc6300a64e087a165
sloria <sloria1@gmail.com>
2014-07-04 14:32:51 -0400

    Add python 2/3 compatibility module

---
+++ mkdocs/_compat.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+"""Python 2/3 compatibility module."""
+import sys
+
+PY2 = int(sys.version[0]) == 2
+
+if PY2:
+    from urlparse import urljoin, urlparse, urlunparse
+    urljoin = urljoin
+    urlparse = urlparse
+    urlunparse = urlunparse
+
+    text_type = unicode
+    binary_type = str
+    string_types = (str, unicode)
+    unicode = unicode
+    basestring = basestring
+else:  # PY3
+    from urllib.parse import urljoin, urlparse, urlunparse
+    urljoin = urljoin
+    urlparse = urlparse
+    urlunparse = urlunparse
+
+    text_type = str
+    binary_type = bytes
+    string_types = (str,)
+    unicode = str
+    basestring = (str, bytes)
