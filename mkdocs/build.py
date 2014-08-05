e75bd3f3d3281b83c1faf55091d5c664c11cad13
Lynn Root <econchick@users.noreply.github.com>
2014-08-05 11:52:54 -0700

    Added Pygments extension

--- mkdocs/build.py
+++ mkdocs/build.py
@@ -53,7 +53,7 @@ def convert_markdown(markdown_source):
     markdown_source = toc.pre_process(markdown_source)
 
     # Generate the HTML from the markdown source
-    md = markdown.Markdown(extensions=['meta', 'toc', 'tables', 'fenced_code'])
+    md = markdown.Markdown(extensions=['meta', 'toc', 'tables', 'fenced_code', 'codehilite'])
     html_content = md.convert(markdown_source)
     meta = md.Meta
 
