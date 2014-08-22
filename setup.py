27b32689317a87b0df4acc4f5a37b0ec3286eb70
Dougal Matthews <dougal@redhat.com>
2014-08-22 08:22:17 +0100

    Relax version requirements to accept a greater version
    Fixes #104

--- setup.py
+++ setup.py
@@ -16,11 +16,11 @@ author = 'Tom Christie'
 author_email = 'tom@tomchristie.com'
 license = 'BSD'
 install_requires = [
-    'Jinja2==2.7.1',
-    'Markdown==2.3.1',
-    'PyYAML==3.10',
-    'watchdog==0.7.0',
-    'ghp-import==0.4.1'
+    'Jinja2>=2.7.1',
+    'Markdown>=2.3.1,<2.5',
+    'PyYAML>=3.10',
+    'watchdog>=0.7.0',
+    'ghp-import>=0.4.1'
 ]
 
 long_description = """Work in progress."""
