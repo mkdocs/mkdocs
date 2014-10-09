7df0c9c2832968ea1ea83f770d733edb6d3a17eb
Dougal Matthews <dougal@redhat.com>
2014-10-09 09:47:58 +0100

    Updated classifiers for PyPI

--- setup.py
+++ setup.py
@@ -87,13 +87,19 @@ setup(
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
         'Environment :: Web Environment',
-        'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Topic :: Internet :: WWW/HTTP',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Documentation',
+        'Topic :: Text Processing',
     ]
 )
