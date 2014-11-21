f10deeb10ee30829344092577467949b6ddfe92d
Eric Hulser <ehulser@teslamotors.com>
2014-11-21 09:54:35 -0800

    moved the contrib packages to their own projects (mkdocs_autodoc, mkdocs_tree repsectively)

--- mkdocs/contrib/tree/config.py
+++
@@ -1,9 +0,0 @@
-"""
-Defines the global configuration options that will be copied from the root config for this
-tree generation.
-
-Avaiable configuration settings are:
-
-    docs_dir # 'docs' by default
-"""
-docs_dir = 'docs'
