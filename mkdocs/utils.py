1237be30d67264fd9af49f9b690608b3ca578d85
Dougal Matthews <dougal@redhat.com>
2014-11-28 09:14:27 +0000

    Skip dotfiles when copying media
    Fixes #245

--- mkdocs/utils.py
+++ mkdocs/utils.py
@@ -55,6 +55,10 @@ def copy_media_files(from_dir, to_dir):
         output_dir = os.path.normpath(os.path.join(to_dir, relative_path))
 
         for filename in filenames:
+
+            if filename.startswith('.'):
+                continue
+
             if not is_markdown_file(filename) and not is_html_file(filename):
                 source_path = os.path.join(source_dir, filename)
                 output_path = os.path.join(output_dir, filename)
