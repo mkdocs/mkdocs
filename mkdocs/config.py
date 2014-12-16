951c8d1d47227462cd23f6b52499717e5d25a42a
kaiix <kvn.hou@gmail.com>
2014-12-17 00:08:50 +0800

    Fixes #283. If the mkdocs.yml is completely empty there is a traceback

--- mkdocs/config.py
+++ mkdocs/config.py
@@ -72,14 +72,15 @@ DEFAULT_CONFIG = {
 
 
 def load_config(filename='mkdocs.yml', options=None):
-    options = options or {}
-    if 'config' in options:
-        filename = options['config']
+    user_config = options or {}
+    if 'config' in user_config:
+        filename = user_config.pop('config')
     if not os.path.exists(filename):
         raise ConfigurationError("Config file '%s' does not exist." % filename)
     with open(filename, 'r') as fp:
-        user_config = yaml.load(fp)
-    user_config.update(options)
+        local_config = yaml.load(fp)
+    if local_config:
+        user_config.update(local_config)
     return validate_config(user_config)
 
 
