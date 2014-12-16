092099c787c401f001a662f89970b9fbd599d4f7
Dougal Matthews <dougal@dougalmatthews.com>
2014-12-16 16:29:27 +0000

    Catch errors in the Watchdog error handler
    Fixes #164

--- mkdocs/serve.py
+++ mkdocs/serve.py
@@ -34,8 +34,11 @@ class ConfigEventHandler(BuildEventHandler):
     Perform a rebuild when the config file changes.
     """
     def on_any_event(self, event):
-        if os.path.basename(event.src_path) == 'mkdocs.yml':
-            super(ConfigEventHandler, self).on_any_event(event)
+        try:
+            if os.path.basename(event.src_path) == 'mkdocs.yml':
+                super(ConfigEventHandler, self).on_any_event(event)
+        except Exception as e:
+            print(e)
 
 
 class FixedDirectoryHandler(httpserver.SimpleHTTPRequestHandler):
