8b7590b402ed9cb7ef665540b9aaa69053b34091
Dougal Matthews <dougal@dougalmatthews.com>
2015-01-08 21:06:22 +0000

    Add some comments to the JavaScript template

--- mkdocs/assets/search/mkdocs/js/tipuesearch_content.js
+++ mkdocs/assets/search/mkdocs/js/tipuesearch_content.js
@@ -1,4 +1,13 @@
+/*
+ * This contains the full search index for tipuesearch. which is
+ * essentially a large JSON object. See mre about the structure
+ * at http://www.tipue.com/search/docs/
+ */
 var tipuesearch = {{ search_index }};
+/*
+ * To make the search index work well on all pages, re-write the
+ * paths to be relative to the current page.
+ */
 for (var i=0; i < tipuesearch.pages.length; i++){
     tipuesearch.pages[i].loc = base_url + tipuesearch.pages[i].loc;
 }
