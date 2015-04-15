bc8518c78db8499c62c1668e01e6013db0174f5f
Dougal Matthews <dougal@redhat.com>
2015-04-15 20:52:15 +0100

    Generate a search index
    This change adds a new stage to the build process which creates a search
    index based on the contents of the documentation.

--- mkdocs/build.py
+++ mkdocs/build.py
@@ -2,18 +2,19 @@
 from __future__ import print_function
 
 from datetime import datetime
+from io import open
+import logging
+import os
 
 from jinja2.exceptions import TemplateNotFound
-import mkdocs
-from mkdocs import nav, toc, utils
 from mkdocs.compat import urljoin
-from mkdocs.relative_path_ext import RelativePathExtension
 import jinja2
 import json
 import markdown
-import os
-import logging
-from io import open
+
+from mkdocs import nav, search, toc, utils
+from mkdocs.relative_path_ext import RelativePathExtension
+import mkdocs
 
 log = logging.getLogger('mkdocs')
 
@@ -96,7 +97,6 @@ def get_global_context(nav, config):
 
         'include_nav': config['include_nav'],
         'include_next_prev': config['include_next_prev'],
-        'include_search': config['include_search'],
 
         'copyright': config['copyright'],
         'google_analytics': config['google_analytics'],
@@ -143,24 +143,31 @@ def get_page_context(page, content, toc, meta, config):
 
         'current_page': page,
         'previous_page': page.previous_page,
-        'next_page': page.next_page,
+        'next_page': page.next_page
     }
 
 
-def build_404(config, env, site_navigation):
+def build_template(template_name, env, config, site_navigation=None, extra_context=None):
 
     log.debug("Building 404.html page")
 
     try:
-        template = env.get_template('404.html')
+        template = env.get_template(template_name)
     except TemplateNotFound:
-        return
+        return False
 
-    global_context = get_global_context(site_navigation, config)
+    if site_navigation is not None:
+        context = get_global_context(site_navigation, config)
+    else:
+        context = {}
 
-    output_content = template.render(global_context)
-    output_path = os.path.join(config['site_dir'], '404.html')
+    if extra_context is not None:
+        context.update(extra_context)
+
+    output_content = template.render(context)
+    output_path = os.path.join(config['site_dir'], template_name)
     utils.write_file(output_content.encode('utf-8'), output_path)
+    return True
 
 
 def _build_page(page, config, site_navigation, env, dump_json):
@@ -208,6 +215,8 @@ def _build_page(page, config, site_navigation, env, dump_json):
     else:
         utils.write_file(output_content.encode('utf-8'), output_path)
 
+    return html_content, table_of_contents, meta
+
 
 def build_pages(config, dump_json=False):
     """
@@ -216,17 +225,30 @@ def build_pages(config, dump_json=False):
     site_navigation = nav.SiteNavigation(config['pages'], config['use_directory_urls'])
     loader = jinja2.FileSystemLoader(config['theme_dir'])
     env = jinja2.Environment(loader=loader)
+    search_index = search.SearchIndex()
 
-    build_404(config, env, site_navigation)
+    build_template('404.html', env, config, site_navigation)
+
+    if not build_template('search.html', env, config, site_navigation):
+        log.debug("Search is enabled but the theme doesn't contain a "
+                  "search.html file. Assuming the theme implements search "
+                  "within a modal.")
 
     for page in site_navigation.walk_pages():
 
         try:
             log.debug("Building page %s", page.input_path)
-            _build_page(page, config, site_navigation, env, dump_json)
+            html_content, table_of_contents, meta = _build_page(
+                page, config, site_navigation, env, dump_json)
+            search_index.add_entry_from_context(
+                page, html_content, table_of_contents)
         except:
             log.error("Error building page %s", page.input_path)
             raise
+
+    search_index = search_index.generate_search_index()
+    json_output_path = os.path.join(config['site_dir'], 'mkdocs', 'search_index.json')
+    utils.write_file(search_index.encode('utf-8'), json_output_path)
 
 
 def build(config, live_server=False, dump_json=False, clean_site_dir=False):
--- mkdocs/tests/base.py
+++ mkdocs/tests/base.py
@@ -1,5 +1,15 @@
 import textwrap
+import markdown
+
+from mkdocs import toc
 
 
 def dedent(text):
     return textwrap.dedent(text).strip()
+
+
+def markdown_to_toc(markdown_source):
+    md = markdown.Markdown(extensions=['toc'])
+    md.convert(markdown_source)
+    toc_output = md.toc
+    return toc.TableOfContents(toc_output)
