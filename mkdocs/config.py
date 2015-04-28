801a5259b7d0f75dbc805d0a4c492f12aec9f8bf
facelessuser <faceless.shop@gmail.com>
2015-04-27 20:34:09 -0600

    Preserve order of Python Markdown extensions
    Python Markdown inclusion order can be important when including various
    custom extensions.  Conflicts can arise as to where extensions get
    inserted in the workflow.  By preserving order of how extensions are
    defined in the settings file, this can allow a user to control
    insertion order and work around these issues.  As Py26 supported is
    noted to be removed in the future, I saw no reason to add additional
    dependencies for OrderedDict.  Try catch statements can be removed when
    Py26 support is removed.

--- mkdocs/config.py
+++ mkdocs/config.py
@@ -5,7 +5,6 @@ import logging
 import ntpath
 import os
 
-import yaml
 from six.moves.urllib.parse import urlparse
 
 from mkdocs import utils
@@ -81,7 +80,7 @@ def load_config(filename='mkdocs.yml', options=None):
     if not os.path.exists(filename):
         raise ConfigurationError("Config file '%s' does not exist." % filename)
     with open(filename, 'r', encoding='utf-8') as fp:
-        user_config = yaml.load(fp)
+        user_config = utils.yaml_load(fp)
         if not isinstance(user_config, dict):
             raise ConfigurationError("The mkdocs.yml file is invalid. See http://www.mkdocs.org/user-guide/configuration/ for more information.")
     user_config.update(options)
