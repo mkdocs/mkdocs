98928f803c38060811993609eb106a3127a9d3bd
Dougal Matthews <dougal@redhat.com>
2015-05-10 13:07:47 +0100

    Re-write the failing tests

--- mkdocs/tests/config/base_tests.py
+++ mkdocs/tests/config/base_tests.py
@@ -11,7 +11,7 @@ class ConfigBaseTests(unittest.TestCase):
     def test_unrecognised_keys(self):
 
         c = base.Config(schema=defaults.DEFAULT_SCHEMA)
-        c.update({
+        c.load_dict({
             'not_a_valid_config_option': "test"
         })
 
