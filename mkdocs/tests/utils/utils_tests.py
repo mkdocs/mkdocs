60c15a8005e23484b5b9dc54699a3563ac0f9619
Jeff Schroeder <jeffschroeder@computer.org>
2015-06-08 22:15:38 -0500

    Add future import of with_statement for 2.6 compat

--- mkdocs/tests/utils/utils_tests.py
+++ mkdocs/tests/utils/utils_tests.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from __future__ import unicode_literals
-
 import mock
+from __future__ import unicode_literals, with_statement
 import os
 import unittest
 
