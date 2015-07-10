399e841135eaf1c6cd92623ce69f48c3e8d6e904
Tom Christie <tom@tomchristie.com>
2015-07-10 09:56:49 +0100

    Making the restructure compatible with current config and context

--- mkdocs/commands/build.py
+++ mkdocs/commands/build.py
@@ -18,6 +18,52 @@ import mkdocs
 log = logging.getLogger(__name__)
 
 
+def get_context(page, nav, docs_files, config):
+    url = functools.partial(get_relative_url, from_path=page.file.input_path)
+    #is_active = functools.partial(is_active, current_page=page)
+
+    return {
+        'site_name': config['site_name'],
+        'site_author': config['site_author'],
+        'favicon': url(config['site_favicon']) if config['site_favicon'] else None,
+        'page_description': config['site_description'],
+
+        'repo_url': config['repo_url'],
+        'repo_name': config['repo_name'],
+        'nav': nav,
+        'base_url': url('/'),
+        'homepage_url': url(nav.homepage),
+        'site_url': config['site_url'],
+
+        'extra_css': [url(file.input_path) for file in docs_files.css_files()],
+        'extra_javascript': [url(file.input_path) for file in docs_files.javascript_files()],
+
+        'include_nav': config['include_nav'],
+        'include_next_prev': config['include_next_prev'],
+
+        'copyright': config['copyright'],
+        'google_analytics': config['google_analytics'],
+
+        'mkdocs_version': mkdocs.__version__,
+        'build_date_utc': datetime.datetime.utcnow(),
+
+        'config': config,
+
+        'page_title': page.title,
+        'page_description': config['site_description'] if page is nav.homepage else None,
+
+        'content': page.html,
+        'toc': page.toc,
+        'meta': page.meta,
+
+        'canonical_url': None,  #canonical_url,
+
+        'current_page': page,
+        'previous_page': page.previous,
+        'next_page': page.next
+    }
+
+
 def build(config, live_server=False, dump_json=False, clean_site_dir=False):
     """
     Perform a full site build.
@@ -35,32 +81,13 @@ def build(config, live_server=False, dump_json=False, clean_site_dir=False):
     nav = get_navigation(config['pages'], docs_files)
     build_pages(docs_files)
 
-    global_context = {
-        'nav': nav,
-        'extra': config['extra'],
-        'site_name': config['site_name'],
-        'build': {
-            'mkdocs_version': mkdocs.__version__,
-            'build_date_utc': datetime.datetime.utcnow(),
-        },
-        'media': {
-            'extra_css': [file.input_path for file in docs_files.css_files()],
-            'extra_javascript': [file.input_path for file in docs_files.javascript_files()]
-        }
-    }
-
     loader = jinja2.FileSystemLoader(config['theme_dir'])
     env = jinja2.Environment(loader=loader)
     template = env.get_template('base.html')
 
     for file in docs_files.documentation_pages():
         page = file.page
-        context = {
-            'page': page,
-            'url': functools.partial(get_relative_url, from_path=file.input_path),
-            'is_active': functools.partial(is_active, current_page=page)
-        }
-        context.update(global_context)
+        context = get_context(page, nav, docs_files, config)
         output_content = template.render(context)
         output_path = os.path.join(config['site_dir'], file.output_path)
         write_file(output_content.encode('utf-8'), output_path)
