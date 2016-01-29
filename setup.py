c1878a24a61fc340bde3ae6f2c80fb150120ea0f
Dougal Matthews <dougal@redhat.com>
2016-01-29 17:21:25 +0000

    Add the classifier for Python 3.5

--- setup.py
+++ setup.py
@@ -90,6 +90,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         "Programming Language :: Python :: Implementation :: CPython",
         'Topic :: Documentation',
         'Topic :: Text Processing',
