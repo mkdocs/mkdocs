cbb2eb24600c6d5a13168fc175bd9829cd183249
Jim Porter <jporter@mozilla.com>
2016-02-26 17:23:05 -0600

    Fix the padding above header elements so that hovering over them works correctly; resolves #843

--- mkdocs/themes/mkdocs/js/base.js
+++ mkdocs/themes/mkdocs/js/base.js
@@ -36,6 +36,12 @@ $('body').scrollspy({
     target: '.bs-sidebar',
 });
 
+/* Toggle the `clicky` class on the body when clicking links to let us
+   retrigger CSS animations. See ../css/base.css for more details. */
+$('a').click(function(e) {
+    $('body').toggleClass('clicky');
+});
+
 /* Prevent disabled links from causing a page reload */
 $("li.disabled a").click(function() {
     event.preventDefault();
