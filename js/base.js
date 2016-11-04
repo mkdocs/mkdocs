e1530a4ac7b18cc23d8e5045049ddfded57bcf2e
Dougal Matthews <dougal@redhat.com>
2016-11-04 14:09:40 +0000

    Deployed cbac6e1 with MkDocs version: 0.16.0

--- js/base.js
+++ js/base.js
@@ -29,11 +29,31 @@ $(document).ready(function() {
     // Highlight.js
     hljs.initHighlightingOnLoad();
     $('table').addClass('table table-striped table-hover');
+
+    // Improve the scrollspy behaviour when users click on a TOC item.
+    $(".nav a").on("click", function() {
+        var clicked = this;
+        setTimeout(function() {
+            var active = $('.nav li.active a');
+            active = active[active.length - 1];
+            if (clicked !== active) {
+                $(active).parent().removeClass("active");
+                $(clicked).parent().addClass("active");
+            }
+        }, 50);
+    });
+
 });
 
 
 $('body').scrollspy({
     target: '.bs-sidebar',
+});
+
+/* Toggle the `clicky` class on the body when clicking links to let us
+   retrigger CSS animations. See ../css/base.css for more details. */
+$('a').click(function(e) {
+    $('body').toggleClass('clicky');
 });
 
 /* Prevent disabled links from causing a page reload */
