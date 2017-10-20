e5f2a4ecd98242af0681f3869b610000d16b6f62
Waylan Limberg <waylan.limberg@icloud.com>
2017-10-20 14:22:44 -0400

    Override site_url with dev_addr on local server.
    When serving locally, the `site_url` actually is the `dev_addr`, so the
    config should relfect that. Note that this override must happen after
    config validation so that the proper defaults are evaluated for the
    `dev_addr` setting. It cannot happen as part of "post_validation" as
    there is no way to know the dev server is running within the config
    validation (for example, the dev_addr may not be None as the user could
    have set the `dev_addr` setting in their config file). Overriding it
    from the serve command is the least invasive method and guarantees it
    only happens when the local dev server is being run.
    
    Also cleaned up `dev_addr` docs, which were missleading in some respects
    and actually encouraged using the dev server over a network.
    
    Fixes #1317. 404 Error pages now work on the loval dev server when
    the `site_url` in the config points to a subdir.

--- mkdocs/commands/serve.py
+++ mkdocs/commands/serve.py
@@ -103,7 +103,10 @@ def serve(config_file=None, dev_addr=None, strict=None, theme=None,
             theme=theme,
             theme_dir=theme_dir
         )
+        # Override a few config settings after validation
         config['site_dir'] = tempdir
+        config['site_url'] = 'http://{0}/'.format(config['dev_addr'])
+
         live_server = livereload in ['dirty', 'livereload']
         dirty = livereload == 'dirty'
         build(config, live_server=live_server, dirty=dirty)
