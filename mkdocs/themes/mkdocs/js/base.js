cd396ada133caa39542ef78d411ffa4e9c31bfd8
Yeray Diaz Diaz <yeraydiazdiaz@gmail.com>
2018-01-20 21:24:26 +0000

    Remove addition of `clicky` class to body and animations. (#1387)

--- mkdocs/themes/mkdocs/js/base.js
+++ mkdocs/themes/mkdocs/js/base.js
@@ -78,12 +78,7 @@ $(document).ready(function() {
 
 $('body').scrollspy({
     target: '.bs-sidebar',
-});
-
-/* Toggle the `clicky` class on the body when clicking links to let us
-   retrigger CSS animations. See ../css/base.css for more details. */
-$('a').click(function(e) {
-    $('body').toggleClass('clicky');
+    offset: 100
 });
 
 /* Prevent disabled links from causing a page reload */
