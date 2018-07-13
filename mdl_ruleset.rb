5ebcb5d1cab0d2edfeb022a17936c64ad06c7974
Waylan Limberg <waylan.limberg@icloud.com>
2018-07-13 15:42:23 -0400

    Set MarkdownLint list indent level to 4

--- mdl_ruleset.rb
+++ mdl_ruleset.rb
@@ -6,5 +6,8 @@ rule 'MD013', :line_length => 80, :code_blocks => false, :tables => false
 # Set Ordered list item prefix to "ordered" (use 1. 2. 3. not 1. 1. 1.)
 rule 'MD029', :style => "ordered"
 
+# Set list indent level to 4 which Python-Markdown requires
+rule 'MD007', :indent => 4
+
 # Exclude code block style
 exclude_rule 'MD046'
