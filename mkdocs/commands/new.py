e356bf30b5392c8a198585691c6a3cc04c48fd6d
Francesco Maida <francesco.maida@gmail.com>
2019-09-09 16:27:46 +0200

    Update "new" welcome page.
    The help command is `mkdocs -h`, not `mkdocs help`. Fixes #1859.

--- mkdocs/commands/new.py
+++ mkdocs/commands/new.py
@@ -15,7 +15,7 @@ For full documentation visit [mkdocs.org](https://www.mkdocs.org).
 * `mkdocs new [dir-name]` - Create a new project.
 * `mkdocs serve` - Start the live-reloading docs server.
 * `mkdocs build` - Build the documentation site.
-* `mkdocs help` - Print this help message.
+* `mkdocs -h` - Print help message and exit.
 
 ## Project layout
 
