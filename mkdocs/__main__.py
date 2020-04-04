f451918a10ef213f81c96a3a59d160ea4029ab0e
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-04-05 00:38:24 +0900

    Remove disable_unicode_literals_warning config

--- mkdocs/__main__.py
+++ mkdocs/__main__.py
@@ -18,11 +18,6 @@ from mkdocs.commands import build, gh_deploy, new, serve  # noqa: E402
 
 log = logging.getLogger(__name__)
 
-# Disable the warning that Click displays (as of Click version 5.0) when users
-# use unicode_literals in Python 2.
-# See http://click.pocoo.org/dev/python3/#unicode-literals for more details.
-click.disable_unicode_literals_warning = True
-
 
 class State:
     ''' Maintain logging level.'''
