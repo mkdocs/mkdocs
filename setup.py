d942f9e1ec10261d5b997d3e64dd5eb406b81286
claudioperez <50180406+claudioperez@users.noreply.github.com>
2020-12-22 16:40:34 -0800

    bump version

--- setup.py
+++ setup.py
@@ -29,12 +29,12 @@ def get_version(package):
 
 
 if sys.argv[-1] == 'publish':
-    if os.system("pip freeze | grep wheel"):
-        print("wheel not installed.\nUse `pip install wheel`.\nExiting.")
-        sys.exit()
-    if os.system("pip freeze | grep twine"):
-        print("twine not installed.\nUse `pip install twine`.\nExiting.")
-        sys.exit()
+#    if os.system("pip freeze | grep wheel"):
+#        print("wheel not installed.\nUse `pip install wheel`.\nExiting.")
+#        sys.exit()
+#    if os.system("pip freeze | grep twine"):
+#        print("twine not installed.\nUse `pip install twine`.\nExiting.")
+#        sys.exit()
     os.system("python setup.py sdist bdist_wheel")
     os.system("twine upload dist/*")
     print("You probably want to also tag the version now:")
--- src/elstir/__init__.py
+++ src/elstir/__init__.py
@@ -2,5 +2,5 @@
 
 
 # For acceptable version formats, see https://www.python.org/dev/peps/pep-0440/
-__version__ = '1.1.2'
+__version__ = '0.0.1'
 
