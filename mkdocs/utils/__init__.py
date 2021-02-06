38d7d79a4bc692dff2c457827fe532bb9b5aa854
Oleh Prypin <oleh@pryp.in>
2021-02-06 03:26:45 +0100

    Exclude edge cases where get_relative_url depends on CWD
    When only one of the two passed paths starts with a slash, this function would produce results that depend on (and expose parts of) the actual current working directory.
    In a similar manner, it was also possible to "break out" of the "current directory" by starting one of the paths with `../../..` etc.
    
    Additionally, the fact that paths always end up being resolved relative to the current working directory made this function less efficient than it needs to be.
    Fun fact: `get_relative_url('path_a', 'path_b')` actually ended up looking up how to get to `/current/working/directory/path_a` from `/current/working/directory/path_b`.
    
    Luckily, none of these behaviors can ever actually come into effect (the function is always called without a leading slash and without paths deliberately trying to escape), but the fix is still good to reduce surprise.
    
    *The actual fix* is that we make the leading slash ignored (or quite the opposite - we always add it).
    Now when either of the two arguments try to go up higher than the top level, they just end up at the top level (e.g. `foo/../..` is effectively `.`), only then the "relative" calculation happens.

--- mkdocs/utils/__init__.py
+++ mkdocs/utils/__init__.py
@@ -259,7 +259,7 @@ def get_relative_url(url, other):
         # Remove filename from other url if it has one.
         parts = posixpath.split(other)
         other = parts[0] if '.' in parts[1] else other
-    relurl = posixpath.relpath(url, other)
+    relurl = posixpath.relpath('/' + url, '/' + other)
     return relurl + '/' if url.endswith('/') else relurl
 
 
