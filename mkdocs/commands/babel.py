0c296ad342f5ab9b69249fb81cfab3a83972c46c
Waylan Limberg <waylan.limberg@icloud.com>
2021-04-27 16:05:39 -0400

    Add support for third party themes to babel commands.

--- mkdocs/commands/babel.py
+++ mkdocs/commands/babel.py
@@ -1,16 +1,23 @@
 from distutils.errors import DistutilsOptionError
 from pathlib import Path
-
-from mkdocs import __version__
-
 from babel.messages import frontend as babel
 
-VALID_THEMES = [
-    d.name for d in Path("mkdocs/themes/").iterdir() if d.is_dir() and d.name[0] != "_"
-]
+
+class ThemeMixin:
+    def get_theme_dir(self):
+        ''' Validate theme option and return path to theme's root obtained from entry point. '''
+        if 'mkdocs.themes' not in self.distribution.entry_points:
+            raise DistutilsOptionError("no mkdocs.themes are defined in entry_points")
+        themes = {}
+        for theme in self.distribution.entry_points['mkdocs.themes']:
+            name, path = theme.split('=', 1)
+            themes[name.strip()] = path.replace('.', '/').strip()
+        if self.theme not in themes:
+            raise DistutilsOptionError("you must specify a valid theme name to work on")
+        return themes[self.theme]
 
 
-class compile_catalog(babel.compile_catalog):
+class compile_catalog(babel.compile_catalog, ThemeMixin):
     user_options = babel.compile_catalog.user_options + [
         ("theme=", "t", "theme name to work on"),
     ]
@@ -18,40 +25,37 @@ class compile_catalog(babel.compile_catalog):
     def initialize_options(self):
         super().initialize_options()
         self.theme = None
-        self.statistics = True
 
     def finalize_options(self):
-        if self.theme not in VALID_THEMES:
-            raise DistutilsOptionError("you must specify a valid theme name to work on")
-        self.domain = "messages"
-        self.directory = f"mkdocs/themes/{self.theme}/locales"
+        if self.theme:
+            theme_dir = self.get_theme_dir()
+            self.directory = f"{theme_dir}/locales"
         super().finalize_options()
 
 
-class extract_messages(babel.extract_messages):
+class extract_messages(babel.extract_messages, ThemeMixin):
     user_options = babel.extract_messages.user_options + [
         ("theme=", "t", "theme name to work on"),
     ]
 
     def initialize_options(self):
         super().initialize_options()
-        self.copyright_holder = "MkDocs"
-        self.msgid_bugs_address = "https://github.com/mkdocs/mkdocs/issues"
-        self.no_wrap = True
-        self.project = "MkDocs"
         self.theme = None
-        self.version = ".".join([i for i in __version__.split(".") if "dev" not in i])
 
     def finalize_options(self):
-        if self.theme not in VALID_THEMES:
-            raise DistutilsOptionError("you must specify a valid theme name to work on")
-        self.input_paths = f"mkdocs/themes/{self.theme}"
-        self.mapping_file = "mkdocs/themes/babel.cfg"
-        self.output_file = f"mkdocs/themes/{self.theme}/messages.pot"
+        if not self.project:
+            self.project = self.distribution.get_name()
+        if not self.version:
+            version = self.distribution.get_version()
+            self.version = ".".join([i for i in version.split(".") if "dev" not in i])
+        if self.theme:
+            theme_dir = self.get_theme_dir()
+            self.input_paths = theme_dir
+            self.output_file = f"{theme_dir}/messages.pot"
         super().finalize_options()
 
 
-class init_catalog(babel.init_catalog):
+class init_catalog(babel.init_catalog, ThemeMixin):
     user_options = babel.init_catalog.user_options + [
         ("theme=", "t", "theme name to work on"),
     ]
@@ -61,29 +65,25 @@ class init_catalog(babel.init_catalog):
         self.theme = None
 
     def finalize_options(self):
-        if self.theme not in VALID_THEMES:
-            raise DistutilsOptionError("you must specify a valid theme name to work on")
-        self.domain = "messages"
-        self.input_file = f"mkdocs/themes/{self.theme}/messages.pot"
-        self.output_dir = f"mkdocs/themes/{self.theme}/locales"
+        if self.theme:
+            theme_dir = self.get_theme_dir()
+            self.input_file = f"{theme_dir}/messages.pot"
+            self.output_dir = f"{theme_dir}/locales"
         super().finalize_options()
 
 
-class update_catalog(babel.update_catalog):
+class update_catalog(babel.update_catalog, ThemeMixin):
     user_options = babel.update_catalog.user_options + [
         ("theme=", "t", "theme name to work on"),
     ]
 
     def initialize_options(self):
         super().initialize_options()
-        self.ignore_obsolete = True
         self.theme = None
-        self.update_header_comment = True
 
     def finalize_options(self):
-        if self.theme not in VALID_THEMES:
-            raise DistutilsOptionError("you must specify a valid theme name to work on")
-        self.domain = "messages"
-        self.input_file = f"mkdocs/themes/{self.theme}/messages.pot"
-        self.output_dir = f"mkdocs/themes/{self.theme}/locales"
+        if self.theme:
+            theme_dir = self.get_theme_dir()
+            self.input_file = f"{theme_dir}/messages.pot"
+            self.output_dir = f"{theme_dir}/locales"
         super().finalize_options()
