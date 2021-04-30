f7dfd8ebca3a20891ac697f4ca9cec4a316ba73d
Waylan Limberg <waylan.limberg@icloud.com>
2021-04-30 13:13:31 -0400

    support overrides and defaults for --theme

--- mkdocs/commands/babel.py
+++ mkdocs/commands/babel.py
@@ -15,6 +15,9 @@ class ThemeMixin:
         entry_points = EntryPoint.parse_map(self.distribution.entry_points, self.distribution)
         if 'mkdocs.themes' not in entry_points:
             raise DistutilsOptionError("no mkdocs.themes are defined in entry_points")
+        if self.theme is None and len(entry_points['mkdocs.themes']) == 1:
+            # Default to the only theme defined in entry_points as none specified.
+            self.theme = tuple(entry_points['mkdocs.themes'].keys())[0]
         if self.theme not in entry_points['mkdocs.themes']:
             raise DistutilsOptionError("you must specify a valid theme name to work on")
         theme = entry_points['mkdocs.themes'][self.theme]
@@ -31,7 +34,7 @@ class compile_catalog(babel.compile_catalog, ThemeMixin):
         self.theme = None
 
     def finalize_options(self):
-        if self.theme:
+        if not self.directory:
             theme_dir = self.get_theme_dir()
             self.directory = f"{theme_dir}/locales"
         super().finalize_options()
@@ -54,10 +57,12 @@ class extract_messages(babel.extract_messages, ThemeMixin):
             self.version = ".".join([i for i in version.split(".") if "dev" not in i])
         if not self.mapping_file:
             self.mapping_file = DEFAULT_MAPPING_FILE
-        if self.theme:
+        if not self.input_paths or not self.output_file:
             theme_dir = self.get_theme_dir()
-            self.input_paths = theme_dir
-            self.output_file = f"{theme_dir}/{self.domain}.pot"
+            if not self.input_paths:
+                self.input_paths = theme_dir
+            if not self.output_file:
+                self.output_file = f"{theme_dir}/{self.domain}.pot"
         super().finalize_options()
 
 
@@ -71,10 +76,12 @@ class init_catalog(babel.init_catalog, ThemeMixin):
         self.theme = None
 
     def finalize_options(self):
-        if self.theme:
+        if not self.input_file or not self.output_dir:
             theme_dir = self.get_theme_dir()
-            self.input_file = f"{theme_dir}/{self.domain}.pot"
-            self.output_dir = f"{theme_dir}/locales"
+            if not self.input_file:
+                self.input_file = f"{theme_dir}/{self.domain}.pot"
+            if not self.output_dir:
+                self.output_dir = f"{theme_dir}/locales"
         super().finalize_options()
 
 
@@ -88,8 +95,10 @@ class update_catalog(babel.update_catalog, ThemeMixin):
         self.theme = None
 
     def finalize_options(self):
-        if self.theme:
+        if not self.input_file or not self.output_dir:
             theme_dir = self.get_theme_dir()
-            self.input_file = f"{theme_dir}/{self.domain}.pot"
-            self.output_dir = f"{theme_dir}/locales"
+            if not self.input_file:
+                self.input_file = f"{theme_dir}/{self.domain}.pot"
+            if not self.output_dir:
+                self.output_dir = f"{theme_dir}/locales"
         super().finalize_options()
