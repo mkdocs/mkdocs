ccce7fcaf9ea285d81c79d5d4fe91ba1f624b89b
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2021-08-20 14:38:26 +0200

    Add __repr__ method to File class

--- mkdocs/structure/files.py
+++ mkdocs/structure/files.py
@@ -141,6 +141,9 @@ class File:
     def __ne__(self, other):
         return not self.__eq__(other)
 
+    def __repr__(self):
+        return "File(src_path='{}', url='{}')".format(self.src_path, self.url)
+
     def _get_stem(self):
         """ Return the name of the file without it's extension. """
         filename = os.path.basename(self.src_path)
