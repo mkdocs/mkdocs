c3d809cde23437801975b23d10443704b00ad75a
Ultrabug <ultrabug@gentoo.org>
2021-10-08 14:33:39 +0200

    search plugin: do not crash on missing lang options, inform instead
    - fallback to english on missing search.lang option
    - support search.lang territory pt_BR -> pt

--- mkdocs/contrib/search/__init__.py
+++ mkdocs/contrib/search/__init__.py
@@ -14,19 +14,29 @@ class LangOption(config_options.OptionallyRequired):
     """ Validate Language(s) provided in config are known languages. """
 
     def lang_file_exists(self, lang):
-        path = os.path.join(base_path, 'lunr-language', f'lunr.{lang}.js')
-        return os.path.isfile(path)
+        for lang_part in lang.split("_"):
+            lang_part = lang_part.lower()
+            if os.path.isfile(os.path.join(base_path, 'lunr-language', f'lunr.{lang_part}.js')):
+                return lang_part
+        return False
 
     def run_validation(self, value):
         if isinstance(value, str):
             value = [value]
         elif not isinstance(value, (list, tuple)):
             raise config_options.ValidationError('Expected a list of language codes.')
-        for lang in value:
-            if lang != 'en' and not self.lang_file_exists(lang):
-                raise config_options.ValidationError(
-                    f'"{lang}" is not a supported language code.'
-                )
+        for lang in list(value):
+            if lang != 'en':
+                lang_detected = self.lang_file_exists(lang)
+                if not lang_detected:
+                    log.info(f"Option search.lang '{lang}' is not supported, falling back to 'en'")
+                    if 'en' not in value:
+                        value.remove(lang)
+                        value.append('en')
+                elif lang_detected != lang:
+                    value.remove(lang)
+                    value.append(lang_detected)
+                    log.info(f"Option search.lang '{lang}' switched to '{lang_detected}'")
         return value
 
 
