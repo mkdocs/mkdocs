855c364ff77d3ec8c8d26cc363de3b82d6793857
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2021-10-12 13:23:44 +0200

    Add support for Python 3.10

--- setup.py
+++ setup.py
@@ -96,6 +96,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3 :: Only',
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
