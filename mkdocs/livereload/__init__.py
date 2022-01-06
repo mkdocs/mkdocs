d4565cbed3663903412b6b374ebeb35cfb2f7112
Oleh Prypin <oleh@pryp.in>
2022-01-06 17:31:18 +0200

    Don't inject javascript poll script if livereload is disabled (#2742)

--- mkdocs/livereload/__init__.py
+++ mkdocs/livereload/__init__.py
@@ -244,7 +244,7 @@ class LiveReloadServer(socketserver.ThreadingMixIn, wsgiref.simple_server.WSGISe
                 return []
             return None  # Not found
 
-        if file_path.endswith(".html"):
+        if self._watched_paths and file_path.endswith(".html"):
             with file:
                 content = file.read()
             content = self._inject_js_into_html(content, epoch)
