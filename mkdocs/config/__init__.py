94ca21ec15c253d5682c3974a43467f5d9edb265
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-01-23 10:51:11 -0500

    Bring config_options and defaults into config/__init__.py to get tests passing

--- mkdocs/config/__init__.py
+++ mkdocs/config/__init__.py
@@ -1,4 +1,7 @@
 from mkdocs.config.base import load_config, Config
+from mkdocs.config import config_options, defaults
 
 __all__ = [load_config.__name__,
-           Config.__name__]
+           Config.__name__,
+           config_options.__name__,
+           defaults.__name__]
