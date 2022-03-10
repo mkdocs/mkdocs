6fadbc921ede75b14cc32a985880b9ab3a11e4a4
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2022-03-10 22:18:14 +0100

    Prevent error with future Jinja2 v3.1

--- mkdocs/utils/filters.py
+++ mkdocs/utils/filters.py
@@ -3,7 +3,13 @@ import jinja2
 from mkdocs.utils import normalize_url
 
 
-@jinja2.contextfilter
+jinja2_version_info = tuple(
+    int(n) for n in jinja2.__version__.split('.') if n.isdigit()
+)
+
+pass_context_decorator = jinja2.contextfilter if jinja2_version_info < (3,) else jinja2.pass_context
+
+@pass_context_decorator
 def url_filter(context, value):
     """ A Template filter to normalize URLs. """
     return normalize_url(value, page=context['page'], base=context['base_url'])
