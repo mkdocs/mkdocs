00a71ba0bedbfda11322ea0cdfb365d779429915
Oleh Prypin <oleh@pryp.in>
2022-09-18 16:35:33 +0200

    Move more documentation from Markdown into docstrings

--- mkdocs/exceptions.py
+++ mkdocs/exceptions.py
@@ -4,7 +4,8 @@ from click import ClickException, echo
 
 
 class MkDocsException(ClickException):
-    """Base exceptions for all MkDocs Exceptions"""
+    """The base class which all MkDocs exceptions inherit from. This should
+    not be raised directly. One of the subclasses should be raised instead."""
 
 
 class Abort(MkDocsException):
@@ -15,12 +16,16 @@ class Abort(MkDocsException):
 
 
 class ConfigurationError(MkDocsException):
-    """Error in configuration"""
+    """This error is raised by configuration validation when a validation error
+    is encountered. This error should be raised by any configuration options
+    defined in a plugin's [config_scheme][]."""
 
 
 class BuildError(MkDocsException):
-    """Error during the build process"""
+    """This error may be raised by MkDocs during the build process. Plugins should
+    not raise this error."""
 
 
 class PluginError(BuildError):
-    """Error in a plugin"""
+    """A subclass of [`mkdocs.exceptions.BuildError`][] which can be raised by plugin
+    events."""
