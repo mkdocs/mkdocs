fdd30c0c34b6d15b09f154b9402db1246c5c22c6
Timothée Mazzucotelli <pawamoy@pm.me>
2023-06-07 01:22:59 +0200

    Add `DictOfItems` type for configuration options (#3242)

--- mkdocs/config/config_options.py
+++ mkdocs/config/config_options.py
@@ -13,6 +13,7 @@ from typing import (
     Any,
     Callable,
     Collection,
+    Dict,
     Generic,
     Iterator,
     List,
@@ -197,6 +198,63 @@ class ListOfItems(Generic[T], BaseConfigOption[List[T]]):
             self.option_type.post_validation(fake_config, key_name)
 
         return [fake_config[k] for k in fake_keys]
+
+
+class DictOfItems(Generic[T], BaseConfigOption[Dict[str, T]]):
+    """
+    Validates a dict of items. Keys are always strings.
+
+    E.g. for `config_options.DictOfItems(config_options.Type(int))` a valid item is `{"a": 1, "b": 2}`.
+    """
+
+    required: bool | None = None  # Only for subclasses to set.
+
+    def __init__(self, option_type: BaseConfigOption[T], default=None) -> None:
+        super().__init__()
+        self.default = default
+        self.option_type = option_type
+        self.option_type.warnings = self.warnings
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}: {self.option_type}"
+
+    def pre_validation(self, config: Config, key_name: str):
+        self._config = config
+        self._key_name = key_name
+
+    def run_validation(self, value: object) -> Dict[str, T]:
+        if value is None:
+            if self.required or self.default is None:
+                raise ValidationError("Required configuration not provided.")
+            value = self.default
+        if not isinstance(value, dict):
+            raise ValidationError(f"Expected a dict of items, but a {type(value)} was given.")
+        if not value:  # Optimization for empty list
+            return value
+
+        fake_config = LegacyConfig(())
+        try:
+            fake_config.config_file_path = self._config.config_file_path
+        except AttributeError:
+            pass
+
+        # Emulate a config-like environment for pre_validation and post_validation.
+        fake_config.data = value
+
+        for key in fake_config:
+            self.option_type.pre_validation(fake_config, key)
+        for key in fake_config:
+            if not isinstance(key, str):
+                raise ValidationError(
+                    f"Expected type: {str} for keys, but received: {type(key)} (key={key})"
+                )
+        for key in fake_config:
+            # Specifically not running `validate` to avoid the OptionallyRequired effect.
+            fake_config[key] = self.option_type.run_validation(fake_config[key])
+        for key in fake_config:
+            self.option_type.post_validation(fake_config, key)
+
+        return value
 
 
 class ConfigItems(ListOfItems[LegacyConfig]):
