mkdocs/mkdocs

commit d4d483c7796a84908b2edb745d9e72de28236b87

Print the local URL of pages that are "built only for the preview"
And refactor `LiveReloadServer` to not start listening in the constructor

mkdocs/livereload/__init__.py
@@ -70,7 +70,6 @@ class LiveReloadServer(socketserver.ThreadingMixIn, wsgiref.simple_server.WSGISe
         mount_path: str = "/",
         polling_interval: float = 0.5,
         shutdown_delay: float = 0.25,
-        **kwargs,
     ) -> None:
         self.builder = builder
         self.server_name = host
@@ -88,7 +87,7 @@ class LiveReloadServer(socketserver.ThreadingMixIn, wsgiref.simple_server.WSGISe
         # To allow custom error pages.
         self.error_handler: Callable[[int], bytes | None] = lambda code: None
 
-        super().__init__((host, port), _Handler, **kwargs)
+        super().__init__((host, port), _Handler, bind_and_activate=False)
         self.set_app(self.serve_request)
 
         self._wanted_epoch = _timestamp()  # The version of the site that started building.
@@ -151,15 +150,21 @@ class LiveReloadServer(socketserver.ThreadingMixIn, wsgiref.simple_server.WSGISe
             self.observer.unschedule(self._watch_refs.pop(path))
 
     def serve(self):
-        self.observer.start()
+        try:
+            self.server_bind()
+            self.server_activate()
 
-        paths_str = ", ".join(f"'{_try_relativize_path(path)}'" for path in self._watched_paths)
-        log.info(f"Watching paths for changes: {paths_str}")
+            self.observer.start()
 
-        log.info(f"Serving on {self.url}")
-        self.serve_thread.start()
+            paths_str = ", ".join(f"'{_try_relativize_path(path)}'" for path in self._watched_paths)
+            log.info(f"Watching paths for changes: {paths_str}")
 
-        self._build_loop()
+            log.info(f"Serving on {self.url}")
+            self.serve_thread.start()
+
+            self._build_loop()
+        finally:
+            self.server_close()
 
     def _build_loop(self):
         while True:

mkdocs/commands/build.py
@@ -4,8 +4,8 @@ import gzip
 import logging
 import os
 import time
-from typing import Any, Sequence
-from urllib.parse import urlsplit
+from typing import TYPE_CHECKING, Any, Sequence
+from urllib.parse import urljoin, urlsplit
 
 import jinja2
 from jinja2.exceptions import TemplateNotFound
@@ -18,6 +18,9 @@ from mkdocs.structure.files import File, Files, InclusionLevel, _set_exclusions,
 from mkdocs.structure.nav import Navigation, get_navigation
 from mkdocs.structure.pages import Page
 from mkdocs.utils import DuplicateFilter  # noqa - legacy re-export
+
+if TYPE_CHECKING:
+    from mkdocs.livereload import LiveReloadServer
 
 log = logging.getLogger(__name__)
 
@@ -247,7 +250,9 @@ def _build_page(
         raise
 
 
-def build(config: MkDocsConfig, live_server: bool = False, dirty: bool = False) -> None:
+def build(
+    config: MkDocsConfig, live_server: LiveReloadServer | None = None, dirty: bool = False
+) -> None:
     """Perform a full site build."""
 
     logger = logging.getLogger('mkdocs')
@@ -305,7 +310,8 @@ def build(config: MkDocsConfig, live_server: bool = False, dirty: bool = False) 
         for file in files.documentation_pages(inclusion=inclusion):
             log.debug(f"Reading: {file.src_uri}")
             if file.page is None and file.inclusion.is_excluded():
-                excluded.append(file.src_path)
+                if live_server:
+                    excluded.append(urljoin(live_server.url, file.url))
                 Page(None, file, config)
             assert file.page is not None
             _populate_page(file.page, config, files, dirty)

mkdocs/commands/serve.py
@@ -59,6 +59,11 @@ def serve(
     is_clean = build_type == 'clean'
     is_dirty = build_type == 'dirty'
 
+    config = get_config()
+    config['plugins'].run_event(
+        'startup', command=('build' if is_clean else 'serve'), dirty=is_dirty
+    )
+
     def builder(config: MkDocsConfig | None = None):
         log.info("Building documentation...")
         if config is None:
@@ -73,31 +78,26 @@ def serve(
         # Override a few config settings after validation
         config.site_url = f'http://{config.dev_addr}{mount_path(config)}'
 
-        build(config, live_server=not is_clean, dirty=is_dirty)
+        build(config, live_server=None if is_clean else server, dirty=is_dirty)
 
-    config = get_config()
-    config['plugins'].run_event(
-        'startup', command=('build' if is_clean else 'serve'), dirty=is_dirty
+    host, port = config.dev_addr
+    server = LiveReloadServer(
+        builder=builder, host=host, port=port, root=site_dir, mount_path=mount_path(config)
     )
+
+    def error_handler(code) -> bytes | None:
+        if code in (404, 500):
+            error_page = join(site_dir, f'{code}.html')
+            if isfile(error_page):
+                with open(error_page, 'rb') as f:
+                    return f.read()
+        return None
+
+    server.error_handler = error_handler
 
     try:
         # Perform the initial build
         builder(config)
-
-        host, port = config.dev_addr
-        server = LiveReloadServer(
-            builder=builder, host=host, port=port, root=site_dir, mount_path=mount_path(config)
-        )
-
-        def error_handler(code) -> bytes | None:
-            if code in (404, 500):
-                error_page = join(site_dir, f'{code}.html')
-                if isfile(error_page):
-                    with open(error_page, 'rb') as f:
-                        return f.read()
-            return None
-
-        server.error_handler = error_handler
 
         if livereload:
             # Watch the documentation files, the config file and the theme files.

mkdocs/tests/build_tests.py
@@ -8,6 +8,7 @@ from unittest import mock
 from mkdocs.commands import build
 from mkdocs.config.defaults import MkDocsConfig
 from mkdocs.exceptions import PluginError
+from mkdocs.livereload import LiveReloadServer
 from mkdocs.structure.files import File, Files
 from mkdocs.structure.nav import get_navigation
 from mkdocs.structure.pages import Page
@@ -23,6 +24,13 @@ def build_page(title, path, config, md_src=''):
     # Fake page.read_source()
     page.markdown, page.meta = meta.get_data(md_src)
     return page, files
+
+
+def testing_server(root, builder=lambda: None, mount_path="/"):
+    with mock.patch("socket.socket"):
+        return LiveReloadServer(
+            builder, host="localhost", port=123, root=root, mount_path=mount_path
+        )
 
 
 class BuildTests(PathAssertionMixin, unittest.TestCase):
@@ -594,7 +602,7 @@ class BuildTests(PathAssertionMixin, unittest.TestCase):
             exclude_docs='ba*.md',
         )
 
-        with self.subTest(serve=False):
+        with self.subTest(live_server=None):
             expected_logs = '''
                 INFO:Documentation file 'test/foo.md' contains a link to 'test/bar.md' which is excluded from the built site.
             '''
@@ -604,17 +612,18 @@ class BuildTests(PathAssertionMixin, unittest.TestCase):
             self.assertPathNotExists(site_dir, 'test', 'baz.html')
             self.assertPathNotExists(site_dir, '.zoo.html')
 
-        with self.subTest(serve=True):
+        server = testing_server(site_dir, mount_path='/documentation/')
+        with self.subTest(live_server=server):
             expected_logs = '''
                 INFO:Documentation file 'test/bar.md' contains a link to 'test/baz.md' which is excluded from the built site.
                 INFO:Documentation file 'test/foo.md' contains a link to 'test/bar.md' which is excluded from the built site.
                 INFO:The following pages are being built only for the preview but will be excluded from `mkdocs build` per `exclude_docs`:
-                  - .zoo.md
-                  - test/bar.md
-                  - test/baz.md
+                  - http://localhost:123/documentation/.zoo.html
+                  - http://localhost:123/documentation/test/bar.html
+                  - http://localhost:123/documentation/test/baz.html
             '''
             with self._assert_build_logs(expected_logs):
-                build.build(cfg, live_server=True)
+                build.build(cfg, live_server=server)
 
             foo_path = Path(site_dir, 'test', 'foo.html')
             self.assertTrue(foo_path.is_file())
@@ -636,13 +645,13 @@ class BuildTests(PathAssertionMixin, unittest.TestCase):
     def test_conflicting_readme_and_index(self, site_dir, docs_dir):
         cfg = load_config(docs_dir=docs_dir, site_dir=site_dir, use_directory_urls=False)
 
-        for serve in False, True:
-            with self.subTest(serve=serve):
+        for server in None, testing_server(site_dir):
+            with self.subTest(live_server=server):
                 expected_logs = '''
                     WARNING:Excluding 'foo/README.md' from the site because it conflicts with 'foo/index.md'.
                 '''
                 with self._assert_build_logs(expected_logs):
-                    build.build(cfg, live_server=serve)
+                    build.build(cfg, live_server=server)
 
                 index_path = Path(site_dir, 'foo', 'index.html')
                 self.assertPathIsFile(index_path)
@@ -660,10 +669,10 @@ class BuildTests(PathAssertionMixin, unittest.TestCase):
             docs_dir=docs_dir, site_dir=site_dir, use_directory_urls=False, exclude_docs='index.md'
         )
 
-        for serve in False, True:
-            with self.subTest(serve=serve):
+        for server in None, testing_server(site_dir):
+            with self.subTest(live_server=server):
                 with self._assert_build_logs(''):
-                    build.build(cfg, live_server=serve)
+                    build.build(cfg, live_server=server)
 
                 index_path = Path(site_dir, 'foo', 'index.html')
                 self.assertPathIsFile(index_path)
@@ -690,9 +699,9 @@ class BuildTests(PathAssertionMixin, unittest.TestCase):
             assert f is not None
             config.nav = Path(f.abs_src_path).read_text().splitlines()
 
-        for serve in False, True:
+        for server in None, testing_server(site_dir):
             for exclude in 'full', 'nav', None:
-                with self.subTest(serve=serve, exclude=exclude):
+                with self.subTest(live_server=server, exclude=exclude):
                     cfg = load_config(
                         docs_dir=docs_dir,
                         site_dir=site_dir,
@@ -708,13 +717,13 @@ class BuildTests(PathAssertionMixin, unittest.TestCase):
                             INFO:The following pages exist in the docs directory, but are not included in the "nav" configuration:
                               - SUMMARY.md
                         '''
-                    if exclude == 'full' and serve:
+                    if exclude == 'full' and server:
                         expected_logs = '''
                             INFO:The following pages are being built only for the preview but will be excluded from `mkdocs build` per `exclude_docs`:
-                              - SUMMARY.md
+                              - http://localhost:123/SUMMARY.html
                         '''
                     with self._assert_build_logs(expected_logs):
-                        build.build(cfg, live_server=serve)
+                        build.build(cfg, live_server=server)
 
                     foo_path = Path(site_dir, 'foo.html')
                     self.assertPathIsFile(foo_path)
@@ -724,7 +733,7 @@ class BuildTests(PathAssertionMixin, unittest.TestCase):
                     )
 
                     summary_path = Path(site_dir, 'SUMMARY.html')
-                    if exclude == 'full' and not serve:
+                    if exclude == 'full' and not server:
                         self.assertPathNotExists(summary_path)
                     else:
                         self.assertPathExists(summary_path)

mkdocs/tests/livereload_tests.py
@@ -38,7 +38,6 @@ def testing_server(root, builder=lambda: None, mount_path="/"):
             root=root,
             mount_path=mount_path,
             polling_interval=0.2,
-            bind_and_activate=False,
         )
         server.setup_environ()
     server.observer.start()

commit 0f8ab6bd6fbbf6d22517e1e63e79f0c62e7803a5

Fix livereload shutdown sequence - crash on Windows
Fixes #3305
Regressed in commit d4d483c7796a84908b2edb745d9e72de28236b87

mkdocs/livereload/__init__.py
@@ -152,21 +152,18 @@ class LiveReloadServer(socketserver.ThreadingMixIn, wsgiref.simple_server.WSGISe
             self.observer.unschedule(self._watch_refs.pop(path))
 
     def serve(self):
-        try:
-            self.server_bind()
-            self.server_activate()
+        self.server_bind()
+        self.server_activate()
 
-            self.observer.start()
+        self.observer.start()
 
-            paths_str = ", ".join(f"'{_try_relativize_path(path)}'" for path in self._watched_paths)
-            log.info(f"Watching paths for changes: {paths_str}")
+        paths_str = ", ".join(f"'{_try_relativize_path(path)}'" for path in self._watched_paths)
+        log.info(f"Watching paths for changes: {paths_str}")
 
-            log.info(f"Serving on {self.url}")
-            self.serve_thread.start()
+        log.info(f"Serving on {self.url}")
+        self.serve_thread.start()
 
-            self._build_loop()
-        finally:
-            self.server_close()
+        self._build_loop()
 
     def _build_loop(self):
         while True:
@@ -213,6 +210,7 @@ class LiveReloadServer(socketserver.ThreadingMixIn, wsgiref.simple_server.WSGISe
 
         if self.serve_thread.is_alive():
             super().shutdown()
+        self.server_close()
         if wait:
             self.serve_thread.join()
             self.observer.join()