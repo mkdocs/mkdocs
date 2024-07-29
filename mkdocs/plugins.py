18afe17ed153c796a704fbb31fede0cfbdba6b6f
Carlo Castoldi <carlo.castoldi@outlook.com>
2024-07-29 20:16:22 +0200

    add on_page_template event

--- mkdocs/plugins.py
+++ mkdocs/plugins.py
@@ -398,6 +398,25 @@ class BasePlugin(Generic[SomeConfig]):
         """
         return context
 
+    def on_page_template(
+            self, template: jinja2.Template, template_file: str, page: Page, config: MkDocsConfig, nav: Navigation
+    ) -> jinja2.Template:
+        """
+        The `page_template` event is called after the template for a page is created
+        from a theme and can be used to alter the template for that specific page only.
+
+        Args:
+            template: a Jinja2 [Template](https://jinja.palletsprojects.com/en/latest/api/#jinja2.Template) object
+            template_file: relative or absolute path to an html file used as template
+            page: `mkdocs.structure.pages.Page` instance
+            config: global configuration objet
+            nav: global navigation object
+
+        Returns:
+            a Jinja2 [Template](https://jinja.palletsprojects.com/en/latest/api/#jinja2.Template) object
+        """
+        return template
+
     def on_post_page(self, output: str, /, *, page: Page, config: MkDocsConfig) -> str | None:
         """
         The `post_page` event is called after the template is rendered, but
@@ -641,6 +660,11 @@ class PluginCollection(dict, MutableMapping[str, BasePlugin]):
         self, context: TemplateContext, *, page: Page, config: MkDocsConfig, nav: Navigation
     ) -> TemplateContext:
         return self.run_event('page_context', context, page=page, config=config, nav=nav)
+
+    def on_page_template(
+        self, template: jinja2.Template, *, template_file: str, page: Page, config: MkDocsConfig, nav: Navigation
+    ) -> jinja2.Template:
+        return self.run_event('page_template', template, template_file=template_file, page=page, config=config, nav=nav)
 
     def on_post_page(self, output: str, *, page: Page, config: MkDocsConfig) -> str:
         return self.run_event('post_page', output, page=page, config=config)
